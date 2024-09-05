68d33fbb7851a5f8c4b5e834d49836c8155e0918
Mithun Banerjee <mithunbanerjee632@gmail.com>
2024-09-05 14:31:09 +0600

    multi authentication

---
+++ routes/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::get('check',function(){
+//     echo "Alright";
+// });
+Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin.home')->middleware('is_admin');
